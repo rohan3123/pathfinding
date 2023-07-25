944301747eff4d905bf65c56637034e4f3999fa4
Rohan <N0989380@my.ntu.ac.uk>
2023-07-25 23:22:00 +0100

    crerated function to draw barriers and grid function

--- pathfinding.py
+++ pathfinding.py
@@ -93,6 +93,7 @@ class Spot:
 #function for position 1 and 2 
 def h(p1, p2):
 	x1, y1 = p1
+	x2, y2 = p2
 	return abs(x1 - x2) + abs(y1 - y2)
 
 #function to make path loop
@@ -101,4 +102,23 @@ def reconstruct_path(came_from, current, draw):
 		current = came_from[current]
 		current.make_path()
 		draw()
+
+#fucntion to draw barrioars
+def draw(win, grid, rows, width):
+	win.fill(WHITE)
+
+	for row in grid:
+		for spot in row:
+			spot.draw(win)
+
+	draw_grid(win, rows, width)
+	pygame.display.update()
+
+#function to draw grid
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
 
